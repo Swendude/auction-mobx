184928b59a0f73a1ecf160d5a73c3800e6ad97f8
Swen Mulderij <swenmulderij@gmail.com>
2022-08-25 13:00:54 +0200

    Pre lecture

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { makeObservable, observable, computed, action, flow } from "mobx";
+import { observer } from "mobx-react-lite";
+import "./App.css";
+
+class Counter {
+  mutations: number[];
+
+  constructor() {
+    this.mutations = [0];
+    makeObservable(this, {
+      mutations: observable,
+      increase: action,
+      undo: action,
+      count: computed
+    });
+  }
+
+  get count() {
+    return this.mutations.reduce((prev, next) => prev + next, 0);
+  }
+
+  increase(n: number) {
+    this.mutations = [...this.mutations, n];
+  }
+  undo() {
+    this.mutations = this.mutations.slice(0, this.mutations.length - 1);
+  }
+}
+
+const myCounter = new Counter();
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>{myCounter.count}</p>
+      <p className="history">
+        {myCounter.mutations
+          .map((num) => (num > 0 ? `+${num}` : `${num}`))
+          .join(",")}
+      </p>
+      <button onClick={() => myCounter.increase(1)}>+</button>
+      <button onClick={() => myCounter.increase(5)}>+5</button>
+      <button onClick={() => myCounter.increase(-1)}>-</button>
+      <button onClick={() => myCounter.increase(-5)}>-5</button>
+      <button onClick={() => myCounter.undo()}>undo</button>
     </div>
   );
 }
 
-export default App;
+export default observer(App);
